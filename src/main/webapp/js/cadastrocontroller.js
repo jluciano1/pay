de54a96a9bed410c95e8a4a05f77593f73a43a6b
jluciano1 <jluciano1>
2018-01-30 03:18:01 +0000

    configuração página cadastro

--- src/main/webapp/js/cadastrocontroller.js
+++ src/main/webapp/js/cadastrocontroller.js
@@ -2,7 +2,39 @@
     angular.module('custom.controllers', []);
     
     app.controller('CadastroController', ['$scope', '$http', '$location', '$rootScope', '$window', '$state', '$translate', 'Notification', function ($scope, $http, $location, $rootScope, $window, $state, $translate, Notification) {
+      $scope.message = {};
+      $scope.cadastrar = function () {
+        $scope.message.error = undefined;
+
+        $http({
+            method: 'POST',
+            url: 'api/security/User/cadastrar',
+            data: $.param($scope.user),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).success(handleSuccess).error(handleError);
+      }
       
+      function handleSuccess(data, status, headers, config) {
+        // Store data response on session storage
+        // The session storage will be cleaned when the browser window is closed
+        if(typeof(Storage) !== "undefined") {
+          // save the user data on localStorage
+          sessionStorage.setItem("_u",JSON.stringify(data));
+          $rootScope.session = JSON.parse(sessionStorage._u);
+        } else {
+          // Sorry! No Web Storage support.
+          // The home page may not work if it depends
+          // on the logged user data
+        }
+        
+        // Redirect to home page
+        $state.go("login");
+      }
+
+      function handleError(data, status, headers, config) {
+        var error = status == 401 ? $translate.instant('Login.view.invalidPassword') : data;
+        Notification.error(error);
+      }
     }]);
 
 } (app));
